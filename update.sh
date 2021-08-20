bbd79fe66c4f50b70b360a1a2b4d36fb3f3fdbee
Drant387 <65508581+Drant387@users.noreply.github.com>
2021-08-20 10:23:21 -0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,14 @@
+echo ----- apt update -----
+sudo apt update
+echo ----- apt-get update -----
+sudo apt-get update
+echo ----- apt upgrade -----
+sudo apt upgrade -y
+echo ----- apt-get upgrade -----
+sudo apt-get upgrade -y
+echo ----- apt autoremove -----
+sudo apt autoremove -y
+echo ----- apt-get autoremove -----
+sudo apt-get autoremove -y
+echo ----- DONE! rebooting -----
+sudo reboot
