75194abb982b2f63efff63546eb8ea064e8e88c5
Drant387 <65508581+Drant387@users.noreply.github.com>
2021-09-04 13:24:37 -0700

    jellyfin install script

---
+++ install/jellyfin.sh
@@ -0,0 +1,5 @@
+sudo apt install apt-transport-https gnupg lsb-release
+wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | sudo apt-key add -
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/debian $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+sudo apt update
+sudo apt install jellyfin
