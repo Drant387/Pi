92ef682aaed60dc6e0ebc5f23cb88f4262af05e3
Anthony Crandall <Drant387@gmail.com>
2021-09-04 13:46:37 -0700

    1

--- install/jellyfin2.sh
+++ install/jellyfin2.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+sudo apt install apt-transport-https gnupg lsb-release
+
+wget -O - https://repo.jellyfin.org/debian/jellyfin_team.gpg.key | sudo apt-key add -
+
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/debian $( lsb_release -c -s ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+
+sudo apt update
+
+sudo apt install jellyfin
 
