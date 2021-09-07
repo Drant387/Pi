ef42af8694197d02ee650c3ae7e4d3e9e2ed345c
Anthony Crandall <Drant387@gmail.com>
2021-09-06 18:05:15 -0700

    updating shell script permissions

--- install/transmission.sh
+++ install/transmission.sh
@@ -3,4 +3,25 @@
 sudo apt update
 sudo apt upgrade
 
+sudo apt install transmission-daemon
+
+sudo systemctl stop transmission-daemon
+
+sudo nano /etc/transmission-daemon/settings.json
+
+sudo nano /etc/init.d/transmission-daemon
+
+sudo nano /etc/systemd/system/multi-user.target.wants/transmission-daemon.service
+
+sudo systemctl daemon-reload
+
+sudo chown -R pi:pi /etc/transmission-daemon
+
+sudo mkdir -p /home/pi/.config/transmission-daemon/
+sudo ln -s /etc/transmission-daemon/settings.json /home/pi/.config/transmission-daemon/
+sudo chown -R pi:pi /home/pi/.config/transmission-daemon/
+
+sudo systemctl start transmission-daemon
+
+echo Done.... I hope?
 
