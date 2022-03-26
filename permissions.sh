7d90cd1b8c3755142e49bca26d754fc26149571a
Drant387 <65508581+Drant387@users.noreply.github.com>
2022-03-25 22:02:14 -0700

    Update permissions.sh

--- permissions.sh
+++ permissions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Set Directories to 775
-#sudo find /torrent -type d -exec chmod 775 {} \;
+sudo find /torrent -type d -exec chmod 775 {} \;
 
 #Set Files to 664
-#sudo find /torrent -type f -exec chmod 664 {} \;
+sudo find /torrent -type f -exec chmod 664 {} \;
 
 #Set everything in /torrent to media group
-#sudo chown -R :media /torrent
+sudo chown -R :media /torrent
